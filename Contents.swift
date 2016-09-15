48c2fbac0b9dc913688310db2d660817fb17b3fd
spal7580 <psaineeraj@gmail.com>
2016-09-15 13:44:04 -0600

    update

---
+++ Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Counting Characters.
+let unusualMenagerie = "happy 😀, Sad 😕, mad 😡, sick 😪"
+print("unusualMenagerie has \(unusualMenagerie.characters.count) characters")
+// Prints "unusualMenagerie has 29 characters"
+
+
+
+
+
+// String Interpolation
+let multiplier = 4
+let message = "\(multiplier) times 3 is \(Double(multiplier) * 3)"
+// message is "3 times 2.5 is 7.5"
+
+
+
+
+// Comparing Strings
+let quotation = "I like programming."
+let sameQuotation = "I like programming."
+if quotation == sameQuotation {
+    print("You both like programming.")
+}
+// Prints "These two strings are considered equal"
+
+
+//String Change
+var String = "String"
+String
+
